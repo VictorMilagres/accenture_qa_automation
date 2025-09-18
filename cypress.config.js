d772cdce8ae1f8a592db9a5b0a9f66b211194b4f
Victor Milagres <victor.pmilagres@gmail.com>
2025-09-18 14:00:16 -0300

    add cypress configs

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,29 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  // define the viewport size
+  viewportWidth: 1920,
+  viewportHeight:1080,
+
+  // define if a video is going to be recorded when the test fails
+  video: true,
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+
+    env: {
+      homol: {
+        api_account: "https://demoqa.com/Account/v1",
+        api_bookstore: "https://demoqa.com/BookStore/v1",
+        demoqa: "https://demoqa.com/"
+      },
+      prod: {
+        api_account: "https://demoqa.com/Account/v1",
+        api_bookstore: "https://demoqa.com/BookStore/v1",
+        demoqa: "https://demoqa.com/"
+      }
+    }
   },
 });
